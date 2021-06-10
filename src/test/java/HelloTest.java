61acb846bf4b882b19f7540b7113559733713945
htcaygl <htcguns@gmail.com>
2021-06-10 21:49:44 +0300

    added second update

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,5 +1,6 @@
 public class HelloTest {
     public static void main(String[] args) {
         System.out.println("Hello TEst");
+        System.out.println("About to share project on Github");
     }
 }
